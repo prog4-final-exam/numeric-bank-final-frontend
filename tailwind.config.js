1470be338673c19565898767ec2ad72f42cda58c
Harenabs21 <hei.harena.6@gmail.com>
2024-04-02 23:50:22 +0300

    move dependences

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,css,ts,tsx,jsx,js}",
+  "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
